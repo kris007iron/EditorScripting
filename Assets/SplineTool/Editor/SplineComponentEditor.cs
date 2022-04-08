c73df98896067421de498f3eb54f2717c387254d
Krzysztof M <kris007.iron@gmail.com>
2022-04-08 08:53:55 +0200

    Adding and inserting control points
    Creating 3 methods:
    - ShowClosestPointOnClosedSpline
    -ShowClosestPointOnOpenSpline
    - HandleCommands

--- Assets/SplineTool/Editor/SplineComponentEditor.cs
+++ Assets/SplineTool/Editor/SplineComponentEditor.cs
@@ -7,6 +7,7 @@ using UnityEditor;
 public class SplineComponentEditor : Editor
 {
     int hotIndex = -1;
+    //We need to set it to -1 otherwise in OnSceneGUI we will delete point every frame we dont want do that
     int removeIndex = -1;
 
     private void OnSceneGUI()
@@ -36,11 +37,12 @@ public class SplineComponentEditor : Editor
             //For diferent states of spline diferent actions
             if (spline.closed)
             {
-                //ShowClosestPointOnClosedSpline(points);
+                ShowClosestPointOnClosedSpline(points);
             }
             else
             {
-                //ShowClosestPointOnOpenSpline(points);
+             
+                ShowClosestPointOnOpenSpline(points);
             }
         }
 
@@ -64,7 +66,7 @@ public class SplineComponentEditor : Editor
                     prop.vector3Value = point + delta;
                     spline.ResetIndex();
                 }
-                //HandleCommands(wp);
+                HandleCommands(wp);
             }
             //Also we need to create some cind of buttons for points they will behave like normal but the will have shape of circle
             Handles.color = i == 0 | i == spline.points.Count - 1 ? Color.red : Color.white;
@@ -75,7 +77,122 @@ public class SplineComponentEditor : Editor
             var labelIsOutside = v.z < 0;
             //drawing and index of control point in case we need to debug somethin
             if (!labelIsOutside) Handles.Label(wp, i.ToString());
+            if (removeIndex >= 0 && points.arraySize > 4)
+            {
+                points.DeleteArrayElementAtIndex(removeIndex);
+                spline.ResetIndex();
+            }
+            removeIndex = -1;
+            serializedObject.ApplyModifiedProperties();
         }
+    }
+    void HandleCommands(Vector3 wp)
+    {
+        //We intercept the command here, so that instead of framing the game object which the spline component is attached to, we frame the hot control point.
+        if(Event.current.type == EventType.ExecuteCommand)
+        {
+            SceneView.currentDrawingSceneView.Frame(new Bounds(wp, Vector3.one * 10), false);
+            Event.current.Use();
+        }
+        if(Event.current.type == EventType.KeyDown)
+        {
+            if(Event.current.keyCode == KeyCode.Backspace)
+            {
+                removeIndex = hotIndex;
+                Event.current.Use();
+            }
+        }
+    }
+    //Both methods check for the lefti click to use SerializedProperty API to insert item into list and to sets new cords for it
+    //Also both of them use closest point so we this part of code split out into separate method
+    void ShowClosestPointOnClosedSpline(SerializedProperty points)
+    {
+        //The method is called on shift press one of pair of methods called there on diferent condition depends on if spline is closed or not also it shows the extended line of predicted closed spline
+        var spline = target as SplineComponent;
+        var plane = new Plane(spline.transform.up, spline.transform.position);
+        var ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        float center;
+        if (plane.Raycast(ray, out center))
+        {
+            var hit = ray.origin + ray.direction * center;
+            Handles.DrawWireDisc(hit, spline.transform.up, 5);
+            var p = SearchForClosestPoint(Event.current.mousePosition);
+            var sp = spline.GetNonUniformPoint(p);
+            Handles.DrawLine(hit, sp);
+
+            if(Event.current.type == EventType.MouseDown && Event.current.button == 0 && Event.current.shift)
+            {
+                var i = (Mathf.FloorToInt(p * spline.points.Count) + 2) % spline.points.Count;
+                points.InsertArrayElementAtIndex(i);
+                points.GetArrayElementAtIndex(i).vector3Value = spline.transform.InverseTransformPoint(sp);//from world space to local space
+                serializedObject.ApplyModifiedProperties();
+                hotIndex = i;
+            }
+        }
+    }
+    void ShowClosestPointOnOpenSpline(SerializedProperty points)
+    {
+        var spline = target as SplineComponent;
+        var plane = new Plane(spline.transform.up, spline.transform.position);
+        var ray = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+        float center;
+        if(plane.Raycast(ray, out center))
+        {
+            var hit = ray.origin + ray.direction * center;
+            var discSize = HandleUtility.GetHandleSize(hit);
+            Handles.DrawWireDisc(hit, spline.transform.up, discSize);
+            var p = SearchForClosestPoint(Event.current.mousePosition);
+
+            if ((hit - spline.GetNonUniformPoint(0)).sqrMagnitude < 25) p = 0;
+            if ((hit - spline.GetNonUniformPoint(1)).sqrMagnitude < 25) p = 1;
+
+            var sp = spline.GetNonUniformPoint(p);
+            var extend = Mathf.Approximately(p, 0) || Mathf.Approximately(p, 1);
+
+            Handles.color = extend ? Color.red : Color.white;
+            Handles.DrawLine(hit, sp);            
+            Handles.color = Color.white;
+
+            var i = 1 + Mathf.FloorToInt(p * (spline.points.Count - 3));
+
+            if (Event.current.type == EventType.MouseDown && Event.current.button == 0 && Event.current.shift)
+            {
+                if (extend)
+                {
+                    if (i == spline.points.Count - 2) i++;
+                    points.InsertArrayElementAtIndex(i);
+                    points.GetArrayElementAtIndex(i).vector3Value = spline.transform.InverseTransformPoint(hit);
+                    hotIndex = i;
+                }
+                else
+                {
+                    i++;
+                    points.InsertArrayElementAtIndex(i);
+                    points.GetArrayElementAtIndex(i).vector3Value = spline.transform.InverseTransformPoint(sp);
+                    hotIndex = i;
+                }
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+    float SearchForClosestPoint(Vector2 screenPoint, float A = 0f, float B = 1f, float steps = 1000)
+    {
+        var spline = target as SplineComponent;
+        var smallestDelta = float.MaxValue;
+        var step = (B - A) / steps;
+        var closestI = A;
+        for (var i = 0; i <= steps; i++)
+        {
+            var p = spline.GetNonUniformPoint(i * step);
+            var gp = HandleUtility.WorldToGUIPoint(p);
+            var delta = (screenPoint - gp).sqrMagnitude;
+            if(delta < smallestDelta)
+            {
+                closestI = i;
+                smallestDelta = delta;
+            }
+        }
+        return closestI * step;
     }
     //Basic method for creating custom inspector contains definition for buttons and helbox with info
     public override void OnInspectorGUI()
